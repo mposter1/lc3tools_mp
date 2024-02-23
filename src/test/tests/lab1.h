3df6bd4009b77d0e191abdfce0b4b763daa83c9a
Maxwell <mposter@umass.edu>
2024-02-23 15:06:28 -0500

    Further possible test case points

--- src/test/tests/lab1.h
+++ src/test/tests/lab1.h
@@ -117,7 +117,7 @@ void lab1_setup_private(uint16_t num_tests, uint16_t seed, Tester &tester, bool 
             stream << "0x" << std::setfill('0') << std::setw(sizeof(uint16_t) * 2) << b;
 
             auto test = [a, b, frac](lc3::sim &sim, Tester &tester, double total_points)
-            { Test(a, b, frac, sim, tester, total_points); };
+            { Test(a, b, 1*frac, sim, tester, total_points); };
             tester.registerTest(test_name + "_" + stream.str(), test, 1*frac, true);
         };
 
