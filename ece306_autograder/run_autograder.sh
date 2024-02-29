cb3921bdf70a889b6ff8bc3c80e5007b31d0257a
Maxwell <mposter@umass.edu>
2024-02-28 22:47:11 -0500

    hotfix2

--- ece306_autograder/run_autograder.sh
+++ ece306_autograder/run_autograder.sh
@@ -1,6 +1,6 @@
 lab=$1
 
 /autograder/source/lc3tools_mp/build/bin/./${lab}_private  /autograder/submission/$lab.asm > /autograder/submission/private.out
-/autograder/source/lc3tools_mp/build/bin/./${lab}_public --tester-verbose /autograder/submission/$lab.asm > /autograder/submission/public.out
+/autograder/source/lc3tools_mp/build/bin/./${lab}_public --print-output --tester-verbose /autograder/submission/$lab.asm > /autograder/submission/public.out
 
 python3 run_tests.py
