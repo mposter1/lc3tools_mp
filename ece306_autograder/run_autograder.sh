6391a5b4480487e7f8175c33cbcf7832975ec433
mposter1 <maxwellposter@gmail.com>
2024-03-03 18:28:39 -0500

    lab2.999999 - 5k debug 4

--- ece306_autograder/run_autograder.sh
+++ ece306_autograder/run_autograder.sh
@@ -1,6 +1,6 @@
 lab=$1
 
 /autograder/source/lc3tools_mp/build/bin/./${lab}_private /autograder/submission/$lab.asm > /autograder/submission/private.out
-/autograder/source/lc3tools_mp/build/bin/./${lab}_public --tester-verbose --print-output /autograder/submission/$lab.asm > /autograder/submission/public.out
+/autograder/source/lc3tools_mp/build/bin/./${lab}_public /autograder/submission/$lab.asm > /autograder/submission/public.out
 
 python3 run_tests.py
